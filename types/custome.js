b8ecee4ce68c6a2816b9d0f57e3fb89e39eed37b
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-01-05 00:21:44 +0200

    Array

--- types/custome.js
+++ types/custome.js
@@ -1,36 +1,67 @@
-var Ar=[3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2,-3,-1]
-var Ar1=[1,NaN,3,5,-3]
-var Ar2=[1,7,3]
-var Ar3=[1,undefined,3,5,-3]
-var Ar4=[-1,null,-8,'quesd',-2]
+
+var Ar=[3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2,-3,-1];
+var Ar1=[1,NaN,3,5,-3];
+var Ar2=[1,7,3];
+var Ar3=[1,undefined,3,5,-3];
+var Ar4=[-1,null,-8,'quesd',-2];
 function calculate(Array){
 var sum=0
 var min=0
 var max=0
-var array=parseInt(Array)
+var array=parseInt(Array);
 for (var i=0; Array.length> i; i++) {
-if(!isNaN(Array[i]))sum+=Array[i]
+if(!isNaN(Array[i]))sum+=Array[i];
 }
 
 for (var i=0; Array.length> i; i++) {
-    if(!isNaN(Array[i]))min+=Array[i]
+if (Math.max(...Array[i]));
 }
 
 for (var i=0; Array.length> i; i++) {
-    if(!isNaN(Array[i]))max+=Array[i]
+if (Math.min(...Array[i]));
 }
 
 return{sum: sum,min: min,max: max}
 }
-var Res=calculate(Ar)
-var Res1=calculate(Ar1)
-var Res2=calculate(Ar2)
-var Res3=calculate(Ar3)
-var Res4=calculate(Ar4)
-var resalt=Res+Res1+Res2+Res3+Res4
-console.log(Res) 
-console.log(Res1)
-console.log(Res2)
-console.log(Res3)
-console.log(Res4)
-console.log(resalt)
+var Res=calculate(Ar);
+var Res1=calculate(Ar1);
+var Res2=calculate(Ar2);
+var Res3=calculate(Ar3);
+var Res4=calculate(Ar4);
+var resalt=Res+Res1+Res2+Res3+Res4;
+console.log(Res);
+console.log(Res1);
+console.log(Res2);
+console.log(Res3);
+console.log(Res4);
+console.log(resalt);
+
+/* Это 2 варика как решить ,но я склоняюсь к тому ,что ниже */
+
+var Ar=[3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2,-3,-1];
+var Ar1=[1,NaN,3,5,-3];
+var Ar2=[1,7,3];
+var Ar3=[1,undefined,3,5,-3];
+var Ar4=[-1,null,-8,'quesd',-2];
+function calculate(Array){
+var sum=0;
+var min= (parseInt(Math.min(...Array)))
+var max= (parseInt(Math.max(...Array)))
+var array=parseInt(Array);
+for (var i=0; Array.length> i; i++) {
+if(!isNaN(Array[i]))sum+=Array[i];
+}
+return{sum: sum,min: min,max: max}
+}
+var Res=calculate(Ar);
+var Res1=calculate(Ar1);
+var Res2=calculate(Ar2);
+var Res3=calculate(Ar3);
+var Res4=calculate(Ar4);
+var resalt=Array;
+console.log(Res);
+console.log(Res1);
+console.log(Res2);
+console.log(Res3);
+console.log(Res4);
+console.log(resalt);
