18c500c99d5c90818b4f2084c8c850eb735d5580
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-01-14 19:41:54 +0200

    custom forEach

--- types/custome.js
+++ types/custome.js
@@ -27,3 +27,12 @@ console.log(Res2);
 console.log(Res3);
 console.log(Res4);
 console.log(resalt);
+
+
+// MyForEach
+var items=[3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2,-3,-1]
+var newItems=[]
+for (var i=0; items.length>i;i++) {
+newItems.push(items[i])
+}
+console.log(newItems)
